2070340fed614a9cbee7c16b7fbc864619fdbd61
spyraexim <spyrayt29@gmail.com>
2024-09-03 22:03:34 +0530

    not understand

--- MyHope/api/client.js
+++ MyHope/api/client.js
@@ -1,6 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
-export default axios.create({ baseURL: "http://82.112.227.14:8000/" });
-=======
-export default axios.create({ baseURL: "http://82.112.227.14:8000/" });
->>>>>>> 9c2d60ea77c3fa46b9a308013530faee637fa445
+
+
+export default axios.create({ baseURL: "http://82.112.227.14:8000/" })
