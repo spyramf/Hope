82ea59f3f6b532998b6970e61cf510b58048c37e
spyraexim <spyrayt29@gmail.com>
2024-09-12 13:45:54 +0530

    missing ip add

--- MyHope/api/client.js
+++ MyHope/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://192.168.1.37:8000", // Replace with your backend IP/URL
+  baseURL: "http://82.112.227.14:8000", // Replace with your backend IP/URL
   headers: {
     "Content-Type": "application/json",
   },
